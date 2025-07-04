f86f63a2c2bb3834b1cbdcf462d14a66ec919885
praneeth-coder-2005 <savediskofc@gmail.com>
2025-07-04 10:06:53 +0530

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+# settings.py
+
+API_ID = 123456  # your API ID from my.telegram.org
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+
+SOURCE_CHANNEL = -1001234567890  # channel where files are coming from
+DEST_CHANNEL = -1009876543210    # channel where renamed files will be posted
+
+CUSTOM_PREFIX = "MyMedia_"       # change this to your desired file prefix
+THUMBNAIL_PATH = "thumb.jpg"     # path to your static thumbnail
